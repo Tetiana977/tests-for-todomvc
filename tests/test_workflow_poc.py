b590189c6cfdf0740cec57f16fa4a8eea890acba
Tetiana977 <tatjana.zorenko@gmail.com>
2021-01-23 14:17:06 +0200

    draft poc test after review

--- tests/test_workflow_poc.py
+++ tests/test_workflow_poc.py
@@ -5,7 +5,10 @@ from selene.support.shared.jquery_style import s, ss
 
 def test_todos_management():
     browser.open('https://todomvc4tasj.herokuapp.com')
-    browser.should(have.js_returned(True, "return $._data($('#clear-completed')[0],'events').hasOwnProperty('click')"))
+    browser.should(have.js_returned(True, "return $._data($("
+                                          "'#clear-completed')[0],"
+                                          "'events').hasOwnProperty("
+                                          "'click')"))
     browser.config.set_value_by_js = True
 
     s('#new-todo').type('a').press_enter()
@@ -14,14 +17,18 @@ def test_todos_management():
     ss('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 
     ss('#todo-list>li').element_by(have.exact_text('c')).double_click()
-    ss('#todo-list>li').element_by(have.css_class('editing')).element('.edit').set_value('c edited').press_escape()
+    ss('#todo-list>li').element_by(have.css_class('editing')).element('.edit')\
+        .set_value('c to be canceled').press_escape()
 
-    ss('#todo-list>li').element_by(have.exact_text('c')).element('.destroy').click()
+    ss('#todo-list>li').element_by(have.exact_text('c')).element('.destroy')\
+        .click()
     ss('#todo-list>li').should(have.exact_texts('a', 'b'))
 
     ss('#todo-list>li').element_by(have.exact_text('a')).double_click()
-    ss('#todo-list>li').element_by(have.css_class('editing')).element('.edit').set_value('a edited').press_enter()
+    ss('#todo-list>li').element_by(have.css_class('editing')).element('.edit')\
+        .set_value('a edited').press_enter()
 
-    ss('#todo-list>li').element_by(have.exact_text('a edited')).element('.toggle').click()
+    ss('#todo-list>li').element_by(have.exact_text('a edited'))\
+        .element('.toggle').click()
     s('#clear-completed').click()
     ss('#todo-list>li').should(have.exact_texts('b'))
