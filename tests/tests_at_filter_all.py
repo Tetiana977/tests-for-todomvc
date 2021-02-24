ca071b8f52855f557f2c2bfa6dc36e9cdba1f55b
Tetiana977 <tatjana.zorenko@gmail.com>
2021-02-24 10:23:08 +0200

    draft atomic tests with class

--- tests/tests_at_filter_all.py
+++ tests/tests_at_filter_all.py
@@ -1,192 +1,192 @@
 from selene.support.shared import browser
-from tests_for_todomvc.model import todomvc
+from tests_for_todomvc.model.todomvc import TodoMvcPage
 
 browser.config.set_value_by_js = True
 
 
 def test_create():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.add('c')
-    todomvc.list_should_be('a', 'b', 'c')
-    todomvc.items_left_should_be(3)
+    TodoMvcPage().add('c')
+    TodoMvcPage().list_should_be('a', 'b', 'c')
+    TodoMvcPage().items_left_should_be(3)
 
-    todomvc.follow_filter_active()
-    todomvc.list_should_be('a', 'b', 'c')
+    TodoMvcPage().follow_filter_active()
+    TodoMvcPage().list_should_be('a', 'b', 'c')
 
-    todomvc.follow_filter_completed()
-    todomvc.list_should_be_not()
+    TodoMvcPage().follow_filter_completed()
+    TodoMvcPage().list_should_be_not()
 
 
 def test_edit_by_enter():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.edit_by_enter('a', 'a edited')
-    todomvc.list_should_be('a edited', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().edit_by_enter('a', 'a edited')
+    TodoMvcPage().list_should_be('a edited', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
 
 def test_edit_by_tab():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.edit_by_tab('a', 'a edited')
-    todomvc.list_should_be('a edited', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().edit_by_tab('a', 'a edited')
+    TodoMvcPage().list_should_be('a edited', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
 
 def test_edit_by_click_outside():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.edit_by_click_outside('a', 'a edited')
-    todomvc.list_should_be('a edited', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().edit_by_click_outside('a', 'a edited')
+    TodoMvcPage().list_should_be('a edited', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
 
 def test_cancel_edit_by_escape():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.cancel_editing('a', 'a edited')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().cancel_editing('a', 'a edited')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
 
 def test_complete():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.toggle('a')
-    todomvc.completed_todos_should_be('a')
-    todomvc.active_todos_should_be('b')
-    todomvc.items_left_should_be(1)
+    TodoMvcPage().toggle('a')
+    TodoMvcPage().completed_todos_should_be('a')
+    TodoMvcPage().active_todos_should_be('b')
+    TodoMvcPage().items_left_should_be(1)
 
-    todomvc.follow_filter_active()
-    todomvc.list_should_be('b')
-    todomvc.items_left_should_be(1)
+    TodoMvcPage().follow_filter_active()
+    TodoMvcPage().list_should_be('b')
+    TodoMvcPage().items_left_should_be(1)
 
-    todomvc.follow_filter_completed()
-    todomvc.list_should_be('a')
-    todomvc.items_left_should_be(1)
+    TodoMvcPage().follow_filter_completed()
+    TodoMvcPage().list_should_be('a')
+    TodoMvcPage().items_left_should_be(1)
 
 
 def test_complete_all():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.toggle_all()
-    todomvc.completed_todos_should_be('a', 'b')
-    todomvc.items_left_should_be(0)
+    TodoMvcPage().toggle_all()
+    TodoMvcPage().completed_todos_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(0)
 
-    todomvc.follow_filter_completed()
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(0)
+    TodoMvcPage().follow_filter_completed()
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(0)
 
-    todomvc.follow_filter_active()
-    todomvc.list_should_be_not()
-    todomvc.items_left_should_be(0)
+    TodoMvcPage().follow_filter_active()
+    TodoMvcPage().list_should_be_not()
+    TodoMvcPage().items_left_should_be(0)
 
 
 def test_activate():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.toggle('a')
+    TodoMvcPage().toggle('a')
 
-    todomvc.toggle('a')
-    todomvc.active_todos_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().toggle('a')
+    TodoMvcPage().active_todos_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
 
 def test_activate_all():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.toggle_all()
+    TodoMvcPage().toggle_all()
 
-    todomvc.toggle_all()
-    todomvc.active_todos_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().toggle_all()
+    TodoMvcPage().active_todos_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
 
 def test_delete_by_cross():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.delete('a')
-    todomvc.list_should_be('b')
-    todomvc.items_left_should_be(1)
+    TodoMvcPage().delete('a')
+    TodoMvcPage().list_should_be('b')
+    TodoMvcPage().items_left_should_be(1)
 
-    todomvc.follow_filter_active()
-    todomvc.list_should_be('b')
+    TodoMvcPage().follow_filter_active()
+    TodoMvcPage().list_should_be('b')
 
 
 def test_delete_by_edit():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b', 'c')
-    todomvc.list_should_be('a', 'b', 'c')
-    todomvc.items_left_should_be(3)
+    TodoMvcPage().add('a', 'b', 'c')
+    TodoMvcPage().list_should_be('a', 'b', 'c')
+    TodoMvcPage().items_left_should_be(3)
 
-    todomvc.edit_by_enter('a', '')
-    todomvc.list_should_be('b', 'c')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().edit_by_enter('a', '')
+    TodoMvcPage().list_should_be('b', 'c')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.edit_by_enter('b', '')
-    todomvc.list_should_be('c')
-    todomvc.items_left_should_be(1)
+    TodoMvcPage().edit_by_enter('b', '')
+    TodoMvcPage().list_should_be('c')
+    TodoMvcPage().items_left_should_be(1)
 
-    todomvc.edit_by_click_outside('c', '')
-    todomvc.list_should_be_not()
+    TodoMvcPage().edit_by_click_outside('c', '')
+    TodoMvcPage().list_should_be_not()
 
 
 def test_clear_completed():
-    todomvc.visit()
+    TodoMvcPage().visit()
 
-    todomvc.add('a', 'b')
-    todomvc.list_should_be('a', 'b')
-    todomvc.items_left_should_be(2)
+    TodoMvcPage().add('a', 'b')
+    TodoMvcPage().list_should_be('a', 'b')
+    TodoMvcPage().items_left_should_be(2)
 
-    todomvc.toggle('a')
-    todomvc.clear_completed()
-    todomvc.list_should_be('b')
-    todomvc.items_left_should_be(1)
+    TodoMvcPage().toggle('a')
+    TodoMvcPage().clear_completed()
+    TodoMvcPage().list_should_be('b')
+    TodoMvcPage().items_left_should_be(1)
 
-    todomvc.follow_filter_active()
-    todomvc.list_should_be('b')
+    TodoMvcPage().follow_filter_active()
+    TodoMvcPage().list_should_be('b')
 
-    todomvc.follow_filter_completed()
-    todomvc.list_should_be_not()
+    TodoMvcPage().follow_filter_completed()
+    TodoMvcPage().list_should_be_not()
--- tests_for_todomvc/model/todomvc.py
+++ tests_for_todomvc/model/todomvc.py
@@ -6,81 +6,84 @@ from selene import have
 todos = ss('#todo-list>li')
 
 
-def visit():
-    download_page = "return $._data($('#clear-completed')[0]," \
-                    "'events').hasOwnProperty('click')"
+class TodoMvcPage:
 
-    browser.open('https://todomvc4tasj.herokuapp.com')
-    browser.should(have.js_returned(True, download_page))
+    def visit(self):
+        download_page = "return $._data($('#clear-completed')[0]," \
+                        "'events').hasOwnProperty('click')"
 
+        browser.open('https://todomvc4tasj.herokuapp.com')
+        browser.should(have.js_returned(True, download_page))
+        return self
 
-def add(*names: str):
-    for name in names:
-        s('#new-todo').type(name).press_enter()
+    def add(self, *names: str):
+        for name in names:
+            s('#new-todo').type(name).press_enter()
+        return self
 
+    def start_editing(self, name: str, new_text):
+        todos.element_by(have.exact_text(name)).double_click()
+        return todos.element_by(have.css_class('editing')) \
+            .element('.edit').set_value(new_text)
 
-def start_editing(name: str, new_text):
-    todos.element_by(have.exact_text(name)).double_click()
-    return todos.element_by(have.css_class('editing')).element('.edit') \
-        .set_value(new_text)
+    def cancel_editing(self, name: str, new_text):
+        self.start_editing(name, new_text).press_escape()
+        return self
 
+    def edit_by_enter(self, name: str, new_text):
+        self.start_editing(name, new_text).press_enter()
+        return self
 
-def cancel_editing(name: str, new_text):
-    start_editing(name, new_text).press_escape()
+    def edit_by_tab(self, name: str, new_text):
+        self.start_editing(name, new_text).press_tab()
+        return self
 
+    def edit_by_click_outside(self, name: str, new_text):
+        self.start_editing(name, new_text)
+        return s('footer').click()
 
-def edit_by_enter(name: str, new_text):
-    start_editing(name, new_text).press_enter()
+    def delete(self, name: str):
+        todos.element_by(have.exact_text(name)).hover() \
+            .element('.destroy').click()
+        return self
 
+    def toggle(self, name: str):
+        todos.element_by(have.exact_text(name)).element('.toggle').click()
+        return self
 
-def edit_by_tab(name: str, new_text):
-    start_editing(name, new_text).press_tab()
+    def toggle_all(self):
+        s('#toggle-all').click()
+        return self
 
+    def clear_completed(self):
+        s('#clear-completed').click()
+        return self
 
-def edit_by_click_outside(name: str, new_text):
-    start_editing(name, new_text)
-    return s('footer').click()
+    def list_should_be(self, *names: str):
+        todos.should(have.exact_texts(*names))
+        return self
 
+    def items_left_should_be(self, amount: int):
+        s('#todo-count strong').should(have.exact_text(str(amount)))
+        return self
 
-def delete(name: str):
-    todos.element_by(have.exact_text(name)).hover().element('.destroy').click()
+    def follow_filter_active(self):
+        s('#filters [href = "#/active"]').click()
+        return self
 
+    def follow_filter_completed(self):
+        s('#filters [href = "#/completed"]').click()
+        return self
 
-def toggle(name: str):
-    todos.element_by(have.exact_text(name)).element('.toggle').click()
+    def list_should_be_not(self):
+        todos.filtered_by(be.visible).should(have.size(0))
+        return self
 
+    def completed_todos_should_be(self, *names: str):
+        ss('#todo-list>li.completed').should(have.exact_texts(*names))
+        return self
 
-def toggle_all():
-    s('#toggle-all').click()
-
-
-def clear_completed():
-    s('#clear-completed').click()
-
-
-def list_should_be(*names: str):
-    todos.should(have.exact_texts(*names))
-
-
-def items_left_should_be(amount: int):
-    s('#todo-count strong').should(have.exact_text(str(amount)))
-
-
-def follow_filter_active():
-    s('#filters [href = "#/active"]').click()
-
-
-def follow_filter_completed():
-    s('#filters [href = "#/completed"]').click()
-
-
-def list_should_be_not():
-    todos.filtered_by(be.visible).should(have.size(0))
-
-
-def completed_todos_should_be(*names: str):
-    ss('#todo-list>li.completed').should(have.exact_texts(*names))
-
-
-def active_todos_should_be(*names: str):
-    ss('#todo-list>li:not(.completed)').should(have.exact_texts(*names))
+    def active_todos_should_be(self, *names: str):
+        ss('#todo-list>li:not(.completed)').should(have.exact_texts(
+            *names))
+        return self
